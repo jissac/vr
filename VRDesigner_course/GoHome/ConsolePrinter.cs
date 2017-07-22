8d6455cbedf39a2cb8a27d4a182d3c80c3abd5de
James Issac <jissac@users.noreply.github.com>
2017-07-21 22:57:15 -0400

    Add files via upload

---
+++ VRDesigner_course/GoHome/ConsolePrinter.cs
@@ -0,0 +1,52 @@
+﻿/* Intro to the Unity console and to C#
+ * Pseudo Code:
+ * -Set start location
+ * -Calculate distance from home
+ * -Print distance from home
+ * -Read player's move
+ * -Update location from home
+ * -Repeat
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class ConsolePrinter : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("Welcome to Go Home!");
+		print("Try to find your way back.");
+
+        float location = 5.0f; // Distance in meters
+        float homeLocation = 2.1f;
+        float distance = homeLocation - location;
+        print("Distance:" + distance);
+
+        if (location > homeLocation)
+        {
+            print("Go backward to get home");
+        }
+        else if (location < homeLocation)
+        {
+            print("Go forward to get home");
+        }
+        else if (location == homeLocation)
+        {
+            print("You're home!");
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            print("Left key pressed");
+        }
+		if (Input.GetKeyDown(KeyCode.RightArrow))
+		{
+			print("Right key pressed");
+		}
+    }
+}
